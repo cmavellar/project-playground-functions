ffaef5feecd4f1bbb9ac2d901afaa071e9c6ec1a
Cássia Avellar <c.m.avellar@gmail.com>
2022-04-12 16:39:47 -0300

    Requisito 1 realizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
- 
+function compareTrue(x,y) {
+  if (x === true && y === true) {
+    return true;
+  }
+  else {
+    return false;
+  } 
 }
+// console.log(compareTrue(3,3));
 
 // Desafio 2
 function calcArea() {
